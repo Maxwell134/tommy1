ec6a21b852c27447d98d2e6f78a8586fea685b8d
Maxwell134 <69123967+Maxwell134@users.noreply.github.com>
2023-07-18 01:58:25 +0530

    name changed

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return '<h1>Hello world</h1>'
+    return '<h1>Hello world to Mintu</h1>'
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=80)
