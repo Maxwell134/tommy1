044214fa0eacb3121e58c92054f9a8dd06af788c
Maxwell134 <69123967+Maxwell134@users.noreply.github.com>
2023-08-11 02:24:30 +0530

    Update main.py
    new

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return '<h1>Hello world to Dona2</h1>'
+    return '<h1>Hello world to Dona3</h1>'
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=80)
